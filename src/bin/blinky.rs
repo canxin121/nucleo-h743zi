ccec1c485e894eb661bf7fa3695a13b1b77c41ce
canxin <1969730106@qq.com>
2023-10-29 00:57:43 +0800

    upload(别问,问就是别管)

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -1,13 +1,10 @@
-//! Blinks an LED
+//! 闪烁一个LED灯
 //!
-//! This assumes that LD1 (green) is connected to pb0 and LD3 (red) is connected
-//! to pb14. This assumption is true for the nucleo-h743zi and nucleo-h743zi2
-//! boards.
-
+//! 这个代码假设LD1（绿色）连接到pb0，LD3（红色）连接到pb14。这个假设对于nucleo-h743zi和nucleo-h743zi2这两种开发板是正确的。
 #![no_std]
 #![no_main]
 
-use defmt_rtt as _; // global logger
+use defmt_rtt as _; // 全局日志器
 
 use defmt::info;
 use panic_probe as _;
@@ -18,37 +15,36 @@ use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    info!("Starting blinky");
-
-    // Get access to the device specific peripherals from the peripheral access crate
+    info!("开始闪烁");
+    // 从外设访问包中获取设备特定外设的访问权限
     let dp = stm32h7xx_hal::stm32::Peripherals::take().unwrap();
 
-    // Take ownership over the RCC devices and convert them into the corresponding HAL structs
+    // 获取RCC设备的所有权，并将它们转换为相应的HAL结构体
     let rcc = dp.RCC.constrain();
 
     let pwr = dp.PWR.constrain();
     let pwrcfg = pwr.freeze();
 
-    // Freeze the configuration of all the clocks in the system and
-    // retrieve the Core Clock Distribution and Reset (CCDR) object
+    // 冻结系统中所有时钟的配置，并
+    // 获取核心时钟分配和复位（CCDR）对象
     let ccdr = rcc.freeze(pwrcfg, &dp.SYSCFG);
 
-    // Acquire the GPIOB peripheral
+    // 获取GPIOB外设
     let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
 
-    // Configure gpio B pin 0 as a push-pull output.
+    // 将gpio B引脚0配置为推挽输出。
     let mut ld1 = gpiob.pb0.into_push_pull_output();
 
-    // Configure gpio B pin 14 as a push-pull output.
+    // 将gpio B引脚14配置为推挽输出。
     let mut ld3 = gpiob.pb14.into_push_pull_output();
     ld3.set_high();
 
-    // Configure the timer to trigger an update every second
+    // 配置定时器每秒触发一次更新
     let mut timer = Timer::tim1(dp.TIM1, ccdr.peripheral.TIM1, &ccdr.clocks);
     timer.start(1.Hz());
 
-    // Wait for the timer to trigger an update and change the state of the LED
-    info!("Entering main loop");
+    // 等待定时器触发更新并改变LED灯的状态
+    info!("进入主循环");
     loop {
         ld1.set_high();
         block!(timer.wait()).unwrap();
